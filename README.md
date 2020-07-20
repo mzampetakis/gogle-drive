408985400056e7b767628afaa20ee180bb0e733a
mzampetakis <mzampetakis@gmail.com>
2020-07-21 00:23:18 +0300

    Adds readme and New() func

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # gogle-drive
 A google's drive API consumer layer in go
+
+This package aims to help consuming google's drive API through a layer that provides functionalities to easily use some of the provided capabilities. It connect to Google;s Drive through an OAuth Client.
+
+## Getting Started
+
+Just import this package using 
+
+```go
+import github.com/mzampetakis/gogle-drive
+```
+
+### First Use
+
+Before running your application you must provide a valid json file with the google's API OAuth API client ID.
+
+Head to [https://console.developers.google.com/apis/credentials/oauthclient](https://console.developers.google.com/apis/credentials/oauthclient) and add `Desktop App` as Application type. Then provide a name to distinguish your app and press the Create button.
+
+At the Google's Console [Credentials page](https://console.developers.google.com/apis/credentials) under the `OAuth 2.0 Client IDs` section you must download the client's credentials as a json file and move it into your project.
+
+> Consider adding this file to your .gitignore as well as the token.json that will be generated later on.
+
+On the first run you will be prompted to authenticated your client through a web link. The output at the first run will prompt to visit a link, and after you allow the requested access, paste the provided token.
+
+```
+Go to the following link in your browser. 
+https://accounts.google.com/o/oauth2/auth?.....
+
+Then type here the authorization code: paste_here_the_provided_token
+```
+
+This will produce a token.json in your project's root directory.
+
+### Examples
+A siple example that lists your google's drive files is provided in [examples/gogledriveexample.go](examples/gogledriveexample.go) file. 
+
+To start a new gogledrive's instance simply call:
+
+```go
+gdrive, err := gogledrive.New("path_to/credentials.json")
+```
+Then using the `gdrive` all provided methods are available. 
+
+To Search for files you can use 
+```go
+filterCriteria := gogledrive.ListFilter{}
+assets, err := gdrive.SearchFiles(filterCriteria)
+```
+
+The `gogledrive.ListFilter` provides filters for file name, mime type and folderID to search within.
+For more details on filtering please refer to the official documentation here: 
+[https://developers.google.com/drive/api/v3/reference/query-ref](https://developers.google.com/drive/api/v3/reference/query-ref)
+
+To get a specific file the method `GetFile` is available through
+
+```go
+data, err := gdrive.GetFile("fileID")
+```
+
+You can review the whole package's documentation by running `go doc -all .` at this package's folder.
--- gogledrive.go
+++ gogledrive.go
@@ -1,6 +1,6 @@
 // Package gogledrive provides some easy to use functionalities about the google drive
 // such as searching for assets and downloading files.
-// It uses the credentials.json which can be generated from:
+// It uses the Google oAuth Client which can be generated from:
 // https://console.developers.google.com/apis/credentials
 package gogledrive
 
@@ -12,6 +12,33 @@ import (
 	"google.golang.org/api/drive/v3"
 )
 
+type Gogledrive struct {
+	srv *drive.Service
+}
+
+// New returns a new instance of gogledrive's.
+// It uses the provided credentialsFile to connect to Google OAuth service.
+func New(credentialsFile string) (*Gogledrive, error) {
+	b, err := ioutil.ReadFile(credentialsFile)
+	if err != nil {
+		return nil, errors.New("Unable to read client secret file: " + err.Error())
+	}
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
+	if err != nil {
+		return nil, errors.New("Unable to parse client secret file to config: " + err.Error())
+	}
+	client := getClient(config)
+
+	srv, err := drive.New(client)
+	if err != nil {
+		return nil, errors.New("Unable to retrieve Drive client: " + err.Error())
+	}
+	return &Gogledrive{
+		srv: srv,
+	}, nil
+}
+
 // ListFilter provides the available filters for feedind the SearchFiles()
 // Use mimeType = 'application/vnd.google-apps.folder' to list folders
 type ListFilter struct {
@@ -22,24 +49,9 @@ type ListFilter struct {
 
 // SearchFiles returns a map where key is the file name and value is the FileId
 // that retrieves as search results based on the provided ListFilter
-func SearchFiles(filters ListFilter) (map[string]string, error) {
+func (gdrive *Gogledrive) SearchFiles(filters ListFilter) (map[string]string, error) {
 	filesList := map[string]string{}
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		return filesList, errors.New("Unable to read client secret file: " + err.Error())
-	}
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
-	if err != nil {
-		return filesList, errors.New("Unable to parse client secret file to config: " + err.Error())
-	}
-	client := getClient(config)
-
-	srv, err := drive.New(client)
-	if err != nil {
-		return filesList, errors.New("Unable to retrieve Drive client: " + err.Error())
-	}
-	r, err := srv.Files.List().Q(constructFilterQ(filters)).
+	r, err := gdrive.srv.Files.List().Q(constructFilterQ(filters)).
 		Fields("nextPageToken, files(id, name, mimeType, parents)").Do()
 	if err != nil {
 		return filesList, errors.New("Unable to retrieve files: " + err.Error())
@@ -65,29 +77,10 @@ func constructFilterQ(filters ListFilter) string {
 	return filterQ
 }
 
-// GetFile return a byte[] with the contents of the fileID provided
-func GetFile(fileID string) ([]byte, error) {
+// GetFile returns a byte[] with the contents of the fileID provided
+func (gdrive *Gogledrive) GetFile(fileID string) ([]byte, error) {
 	var fileBuf []byte
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		return fileBuf, errors.New("Unable to read client secret file: " + err.Error())
-	}
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
-	if err != nil {
-		return fileBuf, errors.New("Unable to parse client secret file to config: " + err.Error())
-	}
-	client := getClient(config)
-
-	srv, err := drive.New(client)
-	if err != nil {
-		return fileBuf, errors.New("Unable to retrieve Drive client: " + err.Error())
-	}
-	r := srv.Files.Get(fileID)
-	if err != nil {
-		return fileBuf, errors.New("Unable to retrieve files: " + err.Error())
-	}
-
+	r := gdrive.srv.Files.Get(fileID)
 	downloadResp, err := r.Download()
 	if err != nil {
 		return fileBuf, errors.New("Unable to download files: " + err.Error())
